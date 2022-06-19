0d5407a22f0eb8d9263f9881ec8f5de0ba82037a
Karen <quemojojojo@hotmail.com>
2022-06-19 14:53:18 -0500

    agregué de nuevo editar y cambiar estado

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -148,7 +148,23 @@ export class ProductRoute {
       }
     );
 
-    //búsqueda excluyente
+    this.#router.put("/:id", [verifyToken, validateRol(2,3)], async (req, res) => {//todo:excluir 
+      const response = await this.#services.updateProduct(req.params.id,req.body);
+      response.success
+      ?
+      successfulResponse(res, 200, true, "Product was successfully updated", response.product)
+      :
+      errorResponse(res, response.error);
+    });
+
+    this.#router.put("/change_status/:id", [verifyToken, validateRol(2,3)], async (req, res) => {
+        const response = await this.#services.changeStatus(req.params.id);
+        response.success
+        ?
+        successfulResponse(res, 200, true, "Product was successfully updated", response.product)
+        :
+        errorResponse(res, response.error);
+    });
 
     this.#router.delete(
       "/:id",
