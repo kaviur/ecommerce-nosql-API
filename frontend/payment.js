9922b7dd419424753f23fd22cdad51b13e77efbb
camora1990 <moorales.camilo@gmail.com>
2022-06-19 19:33:52 -0500

    Cliente

---
+++ frontend/payment.js
@@ -0,0 +1,49 @@
+const stripe = Stripe("pk_test_51LCBY7DxZkw2FAlUNWmkq8HElC2w1UY86gTJqPmzRH8Q2h1fGaYz0md9PU9UFpGEVulQsgICiJlllFOuiR4RttbN00PRBt7wEt");
+
+const form = document.getElementById("payment-form")
+const loadPaymentBtn = document.getElementById("loadPayment")
+
+
+loadPaymentBtn.onclick = ()=>{
+    initialize()
+}
+
+let elements;
+
+const initialize = async ()=>{
+    debugger
+    const response = await fetch("http://localhost:8081/api/v1/payment/pay",{
+        credentials:"include"
+    })
+
+    const data = await response.json()
+
+    const clientSecret = data.data
+
+    const appearance = {
+        theme: 'stripe',
+    };
+
+    elements = stripe.elements({ appearance, clientSecret });
+
+    const paymentElement = elements.create("payment");
+    paymentElement.mount("#payment-element");
+}
+
+form.onsubmit= async (event)=>{
+    event.preventDefault()
+    const result = await stripe.confirmPayment({
+        elements,
+        redirect: 'if_required'
+    })
+    if(result.paymentIntent?.status==="succeeded"){
+        // fetch("http://localhost:4000/api/cart/paymentCompleted",{
+        //     method:"POST",
+        //     credentials:"include"
+        // }).then(resp=>resp.json())
+        // .then(console.log)
+        // .catch(console.log)
+    }
+
+    console.log(result)
+}
